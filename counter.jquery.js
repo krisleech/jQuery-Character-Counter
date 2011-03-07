3a51cbe3a683c3f4af7b4c8906dfedc2a1d66146
Kris Leech <kris.leech@interkonect.com>
2011-03-07 19:20:50 +0000

    updated to use a HTML 5 data- attribute

--- counter.jquery.js
+++ counter.jquery.js
@@ -1,26 +1,22 @@
 (function($){
- $.fn.counter = function(options) {
+  $.fn.counter = function() {
+    return this.each(function() {
+      max_length = parseInt($(this).attr('data-max-length'));
 
-  var defaults = {
-   max_length: 255
+      var length = $(this).val().length;               
+      $(this).parent().find('.counter_label').html(max_length-length + ' characters left');
+      // bind on key up event
+      $(this).keyup(function(){
+        // calc length and truncate if needed
+        var new_length = $(this).val().length;
+        if (new_length > max_length-1) {
+          $(this).val($(this).val().substring(0, options.max_length));
+        }
+        // update visual counter
+        $(this).parent().find('.counter_label').html(max_length-new_length + ' characters left');
+      });
+    });
   };
-
-  var options = $.extend(defaults, options);
-    
-  return this.each(function() {
-	
-                var length = $(this).val().length;               
-				$(this).parent().find('.counter_label').html( options.max_length-length + ' characters left');
-                // bind on key up event
-                $(this).keyup(function(){
-				  // calc length and truncate if needed
-                  var new_length = $(this).val().length;
-                  if (new_length > options.max_length-1) {
-                    $(this).val($(this).val().substring(0, options.max_length));
-                  }
-				  // update visual counter
-                  $(this).parent().find('.counter_label').html( options.max_length-new_length + ' characters left');
-                });
-  });
- };
 })(jQuery);
+
+
