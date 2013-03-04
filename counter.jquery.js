c4573ded71c5ba9c654d1848759ff3b58fcd1015
karyboy <karneshmehra@gmail.com>
2013-03-04 20:24:30 +0530

    addClass if limit exceeded

--- counter.jquery.js
+++ counter.jquery.js
@@ -1,22 +1,33 @@
-(function($){
-  $.fn.counter = function() {
-    return this.each(function() {
-      max_length = parseInt($(this).attr('data-max-length'));
+(function ($) {
+    $.fn.counter = function (options) {
+        var defaults={
+            limitExceedClass:''
+        };
+        var options=$.extend({},defaults,options);
+        return this.each(function () {
+            max_length = parseInt($(this).attr('data-max-length'));
 
-      var length = $(this).val().length;               
-      $(this).parent().find('.counter_label').html(max_length-length + ' characters left');
-      // bind on key up event
-      $(this).keyup(function(){
-        // calc length and truncate if needed
-        var new_length = $(this).val().length;
-        if (new_length > max_length-1) {
-          $(this).val($(this).val().substring(0, options.max_length));
-        }
-        // update visual counter
-        $(this).parent().find('.counter_label').html(max_length-new_length + ' characters left');
-      });
-    });
-  };
+            var length = $(this).val().length;
+            $(this).parent().find('.counter_label').html(max_length - length + ' characters left');
+            // bind on key up event
+            $(this).keyup(function () {
+                // calc length and truncate if needed
+                var new_length = $(this).val().length;
+                if (new_length > max_length - 1) {
+                    $(this).parent().find('.counter_label').addClass(options.limitExceedClass);
+                }
+                else {
+                    $(this).parent().find('.counter_label').removeClass(options.limitExceedClass);
+                }
+                // update visual counter
+                $(this).parent().find('.counter_label').html(max_length - new_length + ' characters left');
+            });
+        });
+    };
 })(jQuery);
+$(document).ready(function () {
+    $('textarea').counter();
+
+})
 
 
